9969d68ae196947ec2e7550a527fa163d3101b2e
Charles Houston <charleshouston0@gmail.com>
2025-05-08 15:09:11 +0200

    Select field

--- Collectibles.Frontend/collectibles/src/components/fields/SelectField.tsx
+++ Collectibles.Frontend/collectibles/src/components/fields/SelectField.tsx
@@ -1,29 +1,82 @@
 import {StyleSheet, Text, View} from "react-native";
 import {fontStyles} from "../../styles/fontStyles";
+import { Dropdown } from 'react-native-element-dropdown';
 
 interface SelectFieldProps {
     label: string;
+    data: DropdownData[];
+    placeholder: string;
     mandatory?: boolean;
+    value?: string;
+    setValue: (value: string) => void;
 }
 
-const SelectField : React.FC<SelectFieldProps> = ({ label, mandatory=false }) => {
+interface DropdownData {
+    label: string;
+    value: string;
+}
+
+const SelectField : React.FC<SelectFieldProps> = ({ label, placeholder, data, value, setValue, mandatory=false }) => {
+    
     return (
-        <View style={{ flexDirection: 'column', justifyContent: 'flex-start' }}>
+        <View style={styles.container}>
             <View style={styles.label}>
                 <Text style={fontStyles.L3}>{label}</Text>
                 {mandatory ? <Text style={styles.mandatory}>*</Text> : null }
+            </View>
+            <View style={styles.input}>
+                <Dropdown 
+                    data={data} 
+                    value={value}
+                    labelField={"label"}
+                    valueField={"value"}
+                    placeholder={placeholder}
+                    onChange={item => {
+                        setValue(item?.value);
+                    }}
+                    selectedTextStyle={styles.text}
+                    inputSearchStyle={styles.text}
+                    placeholderStyle={styles.text}
+                    renderItem={(item) => {
+                        return <View style={styles.dropdownItem}>
+                            <Text style={styles.text}>{item.label}</Text>
+                        </View>
+                    }
+                }/>
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        gap: 4
+    },
     label: {
         flexDirection: 'row',
         justifyContent: 'flex-start',
     },
     mandatory: {
         color: '#2A584F',
+    },
+    input: {
+        alignItems: 'stretch',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        paddingHorizontal: 16,
+        borderRadius: 4,
+        borderColor: '#2A584F',
+        borderWidth: 2,
+        height: 48,
+        gap: 16
+    },
+    text: {
+        ...fontStyles.B3,
+    },
+    dropdownItem: {
+        flex: 1, 
+        paddingVertical: 12,
+        paddingHorizontal: 16,
     }
 })
 
