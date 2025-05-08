bb5a9c69fae02898bec811f0af0221b01e2ea9f6
Charles Houston <charleshouston0@gmail.com>
2025-05-08 10:09:30 +0200

    Created Section

---
+++ Collectibles.Frontend/collectibles/src/components/Section.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {Text, View} from "react-native";
+import Button from "./Button";
+import {fontStyles} from "../styles/fontStyles";
+
+interface SectionProps {
+    title: string;
+    children: React.ReactNode;
+}
+
+const Section : React.FC<SectionProps> = ({title, children}) => {
+    
+    return (<View style={{ width: '100%', gap: 16 }}>
+        <View>
+            <Text style={[fontStyles.L2, {color: '#2A584F'}]}>
+                {title}
+            </Text>
+            <View style={{ height: 1, backgroundColor: '#2A584F', marginVertical: 4 }} />
+        </View>
+        {children}
+    </View>)
+}
+
+export default Section;
