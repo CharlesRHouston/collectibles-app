0d23e9899b8e198fc8b4d97fd954896fe5dd8d62
Charles Houston <charleshouston0@gmail.com>
2025-05-08 13:39:31 +0200

    Text area field

--- Collectibles.Frontend/collectibles/src/components/fields/TextAreaField.tsx
+++ Collectibles.Frontend/collectibles/src/components/fields/TextAreaField.tsx
@@ -1,29 +1,51 @@
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
 import {fontStyles} from "../../styles/fontStyles";
+import {AntDesign} from "@expo/vector-icons";
 
 interface TextAreaFieldProps {
     label: string;
-    mandatory?: boolean;
+    value: string;
+    onTextChange: (text: string) => void;
 }
 
-const TextAreaField : React.FC<TextAreaFieldProps> = ({ label, mandatory=false }) => {
+//TODO: text limit
+const TextAreaField : React.FC<TextAreaFieldProps> = ({ label, value, onTextChange }) => {
     return (
-        <View style={{ flexDirection: 'column', justifyContent: 'flex-start' }}>
+        <View style={styles.container}>
             <View style={styles.label}>
                 <Text style={fontStyles.L3}>{label}</Text>
-                {mandatory ? <Text style={styles.mandatory}>*</Text> : null }
+            </View>
+            <View>
+                <TextInput
+                    style={styles.input}
+                    value={value}
+                    onChangeText={onTextChange}
+                    autoCapitalize={"none"}
+                    autoCorrect={false}
+                    multiline={true}
+                />
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        gap: 4
+    },
     label: {
         flexDirection: 'row',
         justifyContent: 'flex-start',
     },
-    mandatory: {
-        color: '#2A584F',
+    input: {
+        ...fontStyles.B3,
+        backgroundColor: 'white',
+        paddingHorizontal: 12,
+        borderRadius: 4,
+        borderColor: '#2A584F',
+        borderWidth: 2,
+        height: 144,
+        paddingTop: 8
     }
 })
 
