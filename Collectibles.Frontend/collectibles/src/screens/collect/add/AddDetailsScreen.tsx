613a39a43ed03db391d1d570cba6c19511efa6a0
Charles Houston <charleshouston0@gmail.com>
2025-05-22 10:48:00 +0200

    Refactored method structure and location

--- Collectibles.Frontend/collectibles/src/screens/collect/add/AddDetailsScreen.tsx
+++ Collectibles.Frontend/collectibles/src/screens/collect/add/AddDetailsScreen.tsx
@@ -1,39 +1,179 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import Screen from "../../../components/Screen";
-import {useNavigation} from "@react-navigation/native";
+import {CompositeNavigationProp, useNavigation} from "@react-navigation/native";
 import {StackNavigationProp} from "@react-navigation/stack";
 import {useCollectContext} from "../../../contexts/CollectContext";
-import {CollectStackList} from "../../../types/stackParamList";
+import {CollectStackList, MainStackList} from "../../../types/stackParamList";
 import Section from "../../../components/Section";
 import DateField from "../../../components/fields/DateField";
 import TextAreaField from "../../../components/fields/TextAreaField";
 import RadioField from "../../../components/fields/RadioField";
 import Button from "../../../components/Button";
-import {fontStyles} from "../../../styles/fontStyles";
-import {onSubmitCollect} from "../../../utils/collect/onSubmitCollect";
-import {validateAddDetails} from "../../../utils/validation/validateCollectForm";
+import {validateForm} from "../../../utils/validation/validateCollectForm";
 import Loading from "../../../components/Loading";
 import {useCollectionContext} from "../../../contexts/CollectionContext";
 import {buttonContainerStyles, buttonStyles} from "../../../styles/buttonStyles";
 import {errorStyles} from "../../../styles/errorStyles";
 import {useUserCollectibleContext} from "../../../contexts/UserCollectibleContext";
+import {BottomTabNavigationProp} from "@react-navigation/bottom-tabs";
+import {Collection} from "../../../types/collection";
+import {PutCollectibleRequest, UserCollectible} from "../../../types/userCollectible";
+import * as FileSystem from "expo-file-system";
+import axios from "axios";
+import ApiService from "../../../services/apiService";
+
+type CollectScreenNavigationProp = CompositeNavigationProp<
+    StackNavigationProp<CollectStackList, 'AddDetails'>,
+    BottomTabNavigationProp<MainStackList>
+>;
 
 const AddDetailsScreen: React.FC = () => {
     const [loading, setLoading] = useState(false);
     const [errors, setErrors] = useState<string[]>([]);
-    const navigation = useNavigation<StackNavigationProp<CollectStackList, 'AddDetails'>>();
     const { form, setForm } = useCollectContext();
-    
+    const navigation = useNavigation<CollectScreenNavigationProp>();
     const { collections } = useCollectionContext();
     const { userCollectibles, setUserCollectibles } = useUserCollectibleContext();
+
+    const getBonusQuestion = () => {
+        const question = collections!
+            .find(collection => collection.id === form?.collectionId)
+            ?.categories
+            ?.flatMap(category => category.collectibles)
+            ?.find(collectible => collectible.id === form.collectibleId)
+            ?.bonus?.question;
+
+        return question ? `Bonus gem: ${question?.toLowerCase()}` : null;
+    }
+
+    const bonusQuestion = getBonusQuestion();
     
-    const question = collections!
-        .find(collection => collection.id === form?.collectionId)
-        ?.categories
-        ?.flatMap(category => category.collectibles)
-        ?.find(collectible => collectible.id === form.collectibleId)
-        ?.bonus?.question;
+    const getCategoryType = (collectionId: string, categoryId: string ) => {
+        const category = collections!
+            .find(collection => collection.id === collectionId)!
+            .categories
+            .find(category => category.id === categoryId)!;
+
+        return category.type;
+    }
+
+    const updateAndResetStates = (request: PutCollectibleRequest) => {
+        setUserCollectibles(prev => {
+            if (prev) {
+                return [
+                    ...prev.filter(item => item.collectibleId !== form.collectibleId),
+                    {
+                        collectibleId: form.collectibleId!,
+                        ...request,
+                    }
+                ]
+            }
+
+            return [
+                {
+                    collectibleId: form.collectibleId!,
+                    ...request,
+                }
+            ]
+        });
+
+        setForm({
+            collectionId: null,
+            categoryId: null,
+            collectibleId: null,
+            imageUrl: null,
+            dateCollected: new Date(),
+            description: null,
+            bonus: null,
+        });
+
+        setErrors([]);
+    }
+
+    const uploadImageToS3 = async (presignedUrl: string, fileUri: string) => {
+        const fileBase64 = await FileSystem.readAsStringAsync(fileUri, {
+            encoding: FileSystem.EncodingType.Base64,
+        });
+
+        const binaryData = base64ToArrayBuffer(fileBase64);
+
+        const response = await axios.put(presignedUrl, binaryData, {
+            headers: {
+                'Content-Type': 'image/jpeg', //TODO: consider other file types
+            },
+        });
+    };
+
+    const base64ToArrayBuffer = (base64: string) => {
+        const binaryString = atob(base64);
+        const len = binaryString.length;
+        const bytes = new Uint8Array(len);
+        for (let i = 0; i < len; i++) {
+            bytes[i] = binaryString.charCodeAt(i);
+        }
+        return bytes;
+    };
+
+    const validateAddDetailsForm = () => {
+        const isValid = validateForm(errors, setErrors, [
+            {
+                condition: form.dateCollected === null,
+                message: "Date must be provided.",
+            },
+            {
+                condition: form.description === null || form.description!.length === 0,
+                message: "Description must be provided.",
+            },
+            {
+                condition: form.bonus === null,
+                message: "Bonus must be selected.",
+            }
+        ]);
+
+        return isValid;
+    }
+    
+    const onSubmitCollect = async () => {
+        setLoading(true)
+        
+        try {
+            const isValid = validateAddDetailsForm();
+
+            if (isValid) {
+                const presignedResponse = await ApiService.getSignedUrlForUpload("test-file"); //TODO: correct fileName
+
+                await uploadImageToS3(presignedResponse.data.data.url, form.imageUrl!);
+
+                const request: PutCollectibleRequest = {
+                    collectionId: form.collectionId!,
+                    categoryId: form.categoryId!,
+                    collectedAt: form.dateCollected!.toISOString(),
+                    active: true,
+                    description: form.description!,
+                    bonusAchieved: form.bonus!,
+                    imageUrl: form.imageUrl ?? "placeholder",
+                    categoryType: getCategoryType(form.collectionId!, form.categoryId!)!
+                }
+                
+                await ApiService.putCollectible(form.collectibleId!, request);
+
+                updateAndResetStates(request);
+
+                navigation.navigate('HomeStack');
+            }
+        } catch (error) {
+            let errorMessage = "An unexpected error occurred.";
+
+            if (axios.isAxiosError(error) && error.response) {
+                errorMessage = error.response.data;
+            }
+
+            setErrors(prev => [...prev, errorMessage]);
+        } finally {
+            setLoading(false)
+        }
+    }
     
     return (<>
         {
@@ -55,9 +195,9 @@ const AddDetailsScreen: React.FC = () => {
                     value={form.description!}
                     onTextChange={(text) => setForm({...form, description: text})} />
                 {
-                    question !== null &&
+                    bonusQuestion !== null &&
                         <RadioField 
-                            label={`Bonus gem: ${question?.toLowerCase()}`} 
+                            label={bonusQuestion} 
                             value={form.bonus} 
                             setValue={(value) => setForm({...form, bonus: value})} 
                             optionOneText={"Yes"} 
@@ -73,24 +213,7 @@ const AddDetailsScreen: React.FC = () => {
                 />
                 <Button
                     label={"Submit"}
-                    onPress={async () => {
-                        setLoading(true)
-                        const isValid = validateAddDetails(form, errors, setErrors);
-                        if (isValid) {
-                            await onSubmitCollect(
-                                form, 
-                                setForm, 
-                                errors, 
-                                setErrors, 
-                                userCollectibles!,
-                                setUserCollectibles,
-                                collections!
-                            );
-                        }
-                        
-                        setLoading(false);
-                        navigation.navigate('ChooseCollectible');//TODO: navigate to collected collectible
-                    }}
+                    onPress={async () => await onSubmitCollect()}
                     type={'primary'}
                 />
             </View>
