81f9e3ddef6a961f9af46e7c9f9a344e27530a43
Charles Houston <charleshouston0@gmail.com>
2025-07-04 13:03:12 +0200

    Use activity indicator while loading

--- Collectibles.Frontend/collectibles/src/components/Loading.tsx
+++ Collectibles.Frontend/collectibles/src/components/Loading.tsx
@@ -1,10 +1,10 @@
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, ActivityIndicator, View} from "react-native";
 import React from "react";
 
 
 const Loading : React.FC = () => {
     return (<View style={styles.loadingOverlay}>
-        <Text>Loading...</Text>
+        <ActivityIndicator size="large" color="#2A584F" />
     </View>)
 }
 
